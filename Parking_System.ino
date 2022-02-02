96f1581179d613c0cf5bb8b9085e8531619f7113
Farmaan <mfmohamed@students.nsbm.ac.lk>
2022-02-02 18:15:05 +0530

    Added exit gate slots decrement

--- Parking_System.ino
+++ Parking_System.ino
@@ -6,6 +6,8 @@ static const int servoPin = 2;
 
 bool detected = false;
 
+int slots = 20;
+
 //WiFi
 #define WIFI_SSID "Farmaan A10s"
 #define WIFI_PASSWORD "ptgp4252"
@@ -24,11 +26,6 @@ void loop(){
   if(reading){
     if(detected==false){
       inputDetected();
-      //opening gate
-      for(int posDegrees = 0; posDegrees <= 90; posDegrees++) {
-        servo1.write(posDegrees);
-        delay(10);
-      }
     }
     else{
       inputHeld();
@@ -38,11 +35,6 @@ void loop(){
     if(detected){
       delay(5000);  //close gate 5 seconds after detecting the vehicle (give time for the vehicle to leave the gate)
       inputRemoved();
-      //closing gate
-      for(int posDegrees = 90; posDegrees >= 0; posDegrees--) {
-        servo1.write(posDegrees);
-        delay(10);
-      }
     }
   }
   delay(20);
@@ -67,6 +59,11 @@ void inputDetected(){
   Serial.println("Object detected");
   Serial.print("Opening gate");
   detected = true;
+  //opening gate
+  for(int posDegrees = 0; posDegrees <= 90; posDegrees++) {
+    servo1.write(posDegrees);
+    delay(10);
+  }
 }
 
 void inputHeld(){
@@ -77,4 +74,11 @@ void inputRemoved(){
   Serial.println("Removed");
   Serial.println("Closing gate");
   detected = false;
+  //closing gate
+  for(int posDegrees = 90; posDegrees >= 0; posDegrees--) {
+    servo1.write(posDegrees);
+    delay(10);
+  }
+  slots++;
+  Serial.println(slots);
 }
